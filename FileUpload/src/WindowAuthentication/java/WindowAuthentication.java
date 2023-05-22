236d2f150c8961a78798ec1b1409925223a9fed1
dhareene98@gmail.com <dhareene98@gmail.com>
2023-05-22 17:30:43 +0530

    Uploaded from Surface

---
+++ FileUpload/src/WindowAuthentication/java/WindowAuthentication.java
@@ -0,0 +1,12 @@
+package WindowAuthentication.java;
+public class WindowAuthentication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriver driver=new ChromeDriver();
+		driver.get
+		
+	}
+
+}
---
+++ FileUpload/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author prave
+ *
+ */
+module FileUpload {
+}
